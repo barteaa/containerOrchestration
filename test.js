aafe063151c593bc8bc8f9ad28bf5bc141e338f3
barteaa <barteaa@protonmail.com>
2024-06-27 13:11:39 +0300

    fixed css issues

---
+++ test.js
@@ -0,0 +1,72 @@
+< script type = "application/json" data - for = "htmlwidget-78de0d8c2e0de6dec95c" > {
+      "x": {
+         "filter": "top",
+         "vertical": false,
+         "filterHTML": "<tr>\n  <td><\/td>\n  <td data-type=\"character\" style=\"vertical-align: top;\">\n    <div class=\"form-group has-feedback\" style=\"margin-bottom: auto;\">\n      <input type=\"search\" placeholder=\"All\" class=\"form-control\" style=\"width: 100%;\" disabled=\"\"/>\n      <span class=\"glyphicon glyphicon-remove-circle form-control-feedback\"><\/span>\n    <\/div>\n  <\/td>\n  <td data-type=\"character\" style=\"vertical-align: top;\">\n    <div class=\"form-group has-feedback\" style=\"margin-bottom: auto;\">\n      <input type=\"search\" placeholder=\"All\" class=\"form-control\" style=\"width: 100%;\"/>\n      <span class=\"glyphicon glyphicon-remove-circle form-control-feedback\"><\/span>\n    <\/div>\n  <\/td>\n  <td data-type=\"character\" style=\"vertical-align: top;\">\n    <div class=\"form-group has-feedback\" style=\"margin-bottom: auto;\">\n      <input type=\"search\" placeholder=\"All\" class=\"form-control\" style=\"width: 100%;\"/>\n      <span class=\"glyphicon glyphicon-remove-circle form-control-feedback\"><\/span>\n    <\/div>\n  <\/td>\n  <td data-type=\"character\" style=\"vertical-align: top;\">\n    <div class=\"form-group has-feedback\" style=\"margin-bottom: auto;\">\n      <input type=\"search\" placeholder=\"All\" class=\"form-control\" style=\"width: 100%;\"/>\n      <span class=\"glyphicon glyphicon-remove-circle form-control-feedback\"><\/span>\n    <\/div>\n  <\/td>\n  <td data-type=\"character\" style=\"vertical-align: top;\">\n    <div class=\"form-group has-feedback\" style=\"margin-bottom: auto;\">\n      <input type=\"search\" placeholder=\"All\" class=\"form-control\" style=\"width: 100%;\"/>\n      <span class=\"glyphicon glyphicon-remove-circle form-control-feedback\"><\/span>\n    <\/div>\n  <\/td>\n  <td data-type=\"character\" style=\"vertical-align: top;\">\n    <div class=\"form-group has-feedback\" style=\"margin-bottom: auto;\">\n      <input type=\"search\" placeholder=\"All\" class=\"form-control\" style=\"width: 100%;\"/>\n      <span class=\"glyphicon glyphicon-remove-circle form-control-feedback\"><\/span>\n    <\/div>\n  <\/td>\n  <td data-type=\"character\" style=\"vertical-align: top;\">\n    <div class=\"form-group has-feedback\" style=\"margin-bottom: auto;\">\n      <input type=\"search\" placeholder=\"All\" class=\"form-control\" style=\"width: 100%;\"/>\n      <span class=\"glyphicon glyphicon-remove-circle form-control-feedback\"><\/span>\n    <\/div>\n  <\/td>\n<\/tr>",
+         "extensions": ["Buttons"],
+         "data": [
+            ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33"],
+            ["VN", "VN", "VN", "VN", "VN", "VN", "VN", "VN", "VN", "VN", "VN", "VN", "VN", "VN", "VN", "VN", "VN", "VN", "VN", "VN", "VN", "VN", "VN", "VN", "VN", "VN", "VN", "VN", "VN", "VN", "VN", "VN", "VN"],
+            ["C000", "100", "101", "102", "103", "104", "105", "199", "300", "499", "599", "200", "202", "203", "204", "205", "206", "207", "208", "209", "210", "211", "212", "299", "21200", "21201", "21202", "201", "270", "280", "161981", "C060", "701"],
+            ["CORPORATE", "AGENCY", "AGENCY - FULL TIME AGENT", "NEST", "NEST - DUMMY", "EXCHANGE", "EXCHANGE - DUMMY", "AGENCY - DUMMY", "COLLECTOR", "QB - DUMMY", "CASHIER - DUMMY", "BANCA", "BANCA - ACB", "BANCA - ANZ", "BANCA - DONG A", "BANCA - HSBC", "BANCA - KIEN LONG BANK", "BANCA - PUBLIC BANK", "BANCA - SEABANK", "BANCA - SHINHAN", "BANCA - TIEN PHONG BANK", "BANCA - VIET CAPITAL BANK", "BANCA - VPBANK", "BANCA - DUMMY", "BANCA - VPBANK - UNCATEGORIZED", "BANCA - VPBANK - LENDING", "BANCA - VPBANK - NON-LENDING", "BANCA - CITIBANK", "IFA/ BROKERAGE", "DIRECT MAKERTING", "PENSION - NO BROKER", "CORPORATE", "DIGITAL PLATFORM PARTNERSHIP - TIKI"],
+            ["C000", "C010", "C010", "C010", "C010", "C010", "C010", "C010", "C010", "C010", "C010", "C020", "C020", "C020", "C020", "C020", "C020", "C020", "C020", "C020", "C020", "C020", "C020", "C020", "C020", "C020", "C020", "C021", "C030", "C040", "C060", "C060", "C070"],
+            ["CORPORATE", "AGENCY", "AGENCY", "AGENCY", "AGENCY", "AGENCY", "AGENCY", "AGENCY", "AGENCY", "AGENCY", "AGENCY", "BANCASSURANCE", "BANCASSURANCE", "BANCASSURANCE", "BANCASSURANCE", "BANCASSURANCE", "BANCASSURANCE", "BANCASSURANCE", "BANCASSURANCE", "BANCASSURANCE", "BANCASSURANCE", "BANCASSURANCE", "BANCASSURANCE", "BANCASSURANCE", "BANCASSURANCE", "BANCASSURANCE", "BANCASSURANCE", "BANCASSURANCE - CITI", "IFA/BROKERAGE - IFA", "DIRECT MKTING - SPONSOR MKTING", "CORPORATE DIRECT SALES", "CORPORATE", "DIGITAL CHANNEL"],
+            ["OTHERS", "AGENCY", "AGENCY", "AGENCY", "AGENCY", "AGENCY", "AGENCY", "AGENCY", "AGENCY", "AGENCY", "AGENCY", "BANCASSURANCE", "BANCASSURANCE", "BANCASSURANCE", "BANCASSURANCE", "BANCASSURANCE", "BANCASSURANCE", "BANCASSURANCE", "BANCASSURANCE", "BANCASSURANCE", "BANCASSURANCE", "BANCASSURANCE", "BANCASSURANCE", "BANCASSURANCE", "BANCASSURANCE", "BANCASSURANCE", "BANCASSURANCE", "BANCASSURANCE", "OTHERS", "OTHERS", "OTHERS", "OTHERS", "DIGITAL"],
+            ["OTHERS", "AGENCY", "AGENCY", "AGENCY", "AGENCY", "AGENCY", "AGENCY", "AGENCY", "AGENCY", "AGENCY", "AGENCY", "Banca", "ACB", "ANZ", "DAB", "HSBC", "KLB", "PLB", "SEA", "SHB", "TPB", "VCAP", "VPB", "Dummy", "VPB", "VPB", "VPB", "CITI", "OTHERS", "OTHERS", "OTHERS", "OTHERS", "TIKI"]
+         ],
+         "container": "<table class=\"display\">\n  <thead>\n    <tr>\n      <th> <\/th>\n      <th>Country Code<\/th>\n      <th>PCCI Code<\/th>\n      <th>PCCI Code Description<\/th>\n      <th>SAP Channel Code<\/th>\n      <th>SAP Channel Code Description<\/th>\n      <th>TYPES<\/th>\n      <th>Detail channel<\/th>\n    <\/tr>\n  <\/thead>\n<\/table>",
+         "options": {
+            "dom": "Bfrtip",
+            "buttons": ["copy", "excel"],
+            "pageLength": 10,
+            "autoWidth": true,
+            "columnDefs": [{
+               "orderable": false,
+               "targets": 0
+            }, {
+               "name": " ",
+               "targets": 0
+            }, {
+               "name": "Country Code",
+               "targets": 1
+            }, {
+               "name": "PCCI Code",
+               "targets": 2
+            }, {
+               "name": "PCCI Code Description",
+               "targets": 3
+            }, {
+               "name": "SAP Channel Code",
+               "targets": 4
+            }, {
+               "name": "SAP Channel Code Description",
+               "targets": 5
+            }, {
+               "name": "TYPES",
+               "targets": 6
+            }, {
+               "name": "Detail channel",
+               "targets": 7
+            }],
+            "order": [],
+            "orderClasses": false,
+            "orderCellsTop": true
+         }
+      },
+      "evals": [],
+      "jsHooks": []
+   } < /script>
+   <script type = "application/htmlwidget-sizing" data - for = "htmlwidget-78de0d8c2e0de6dec95c" > {
+      "viewer": {
+         "width": "100%",
+         "height": "auto",
+         "padding": 15,
+         "fill": true
+      },
+      "browser": {
+         "width": "100%",
+         "height": "auto",
+         "padding": 40,
+         "fill": false
+      }
+   } < /script>
